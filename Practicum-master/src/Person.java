6d3c95a3b648927ac654bc8c7ef834ec286e930b
dalto <dalton.m.towe@gmail.com>
2024-01-22 21:52:59 -0500

    Initial commit

---
+++ Practicum-master/src/Person.java
@@ -0,0 +1,19 @@
+public class Person
+{
+    private String ID;
+    private String firstname;
+    private String lastname;
+    private String title;
+    private int YOB;
+
+    public Person(String ID, String firstname, String lastname, String title, int YOB)
+    {
+        this.ID = ID;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.title = title;
+        this.YOB = YOB;
+    }
+
+
+}
